cd16d6b266f7efe1da5a225a64d90f3fb8c18418
Przybyłowicz Maxwell <74791570+365cent@users.noreply.github.com>
2022-11-28 12:41:26 -0500

    Create worldCupDB.h

---
+++ worldCupDB.h
@@ -0,0 +1,20 @@
+/*==============================================================
+| Name: worldCupDB.c
+| This assignment will ask you to apply the following concepts from class:
+| Basic programming concepts
+| Intermediate concepts added in Assignment 4
+| Linked Lists and memory management
+| Makefiles
+| Program organization
+| This header file contains other two header files: worldcup_team.h and worldcup_player.h
+*/
+
+#ifndef worldCupDB
+#define worldCupDB
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "worldcup_team.h"
+#include "worldcup_player.h"
+#endif
